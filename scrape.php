164364f501e3925bf33f8deebe1c0d0af627c89f
Michael Dyrynda <michaeld@hostworks.com.au>
2015-07-20 13:47:08 +0930

    alias class, remove unused import, one-letter variable

--- scrape.php
+++ scrape.php
@@ -1,4 +1,4 @@
 <?php
 require dirname(__FILE__) . '/vendor/autoload.php';
-use Symfony\Component\DomCrawler\Crawler, Symfony\Component\CssSelector\CssSelector;
-print (($contents = file_get_contents('https://www.packtpub.com/packt/offers/free-learning')) ? trim((new Crawler($contents))->filter('h2')->first()->text()) : 'Could not load URL') . PHP_EOL;
+use Symfony\Component\DomCrawler\Crawler as C;
+print (($c = file_get_contents('https://www.packtpub.com/packt/offers/free-learning')) ? trim((new C($c))->filter('h2')->first()->text()) : 'Could not load URL') . PHP_EOL;
